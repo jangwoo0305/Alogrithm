6d99b2d69d2200f7467fa6dfbbc97efb3043f7d8
JangWoo <tjwkddn970305@gmail.com>
2024-12-26 15:28:06 +0900

    Title: 두 사람, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 241226/두 사람/two-person.py
+++ 241226/두 사람/two-person.py
@@ -3,9 +3,10 @@ age1, gender1 = input().split()
 age = int(age)
 age1 = int(age1)
 
-if age >= 19 or age1 >= 19:
-    if gender == "M" or gender1 == "M":
-        print(1)
-    else:
-        print(0)
+if (age >= 19 and gender == "M") or (age1 >= 19 and gender1 == "M"):
+    print(1)
+else : 
+    print(0)
+   
+
 
