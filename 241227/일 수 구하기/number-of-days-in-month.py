6ddd4722c8e7aadf835143f8726860265ee2dc0c
JangWoo <tjwkddn970305@gmail.com>
2024-12-27 16:40:22 +0900

    Title: 일 수 구하기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241227/일 수 구하기/number-of-days-in-month.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+if n <= 7:
+    if n % 2 == 0:
+        print(30)
+    elif n == 2:
+        print(28)
+    else:
+        print(31)
+
+else:
+    if n % 2 == 0:
+        print(31)
+    else:
+        print(30)
