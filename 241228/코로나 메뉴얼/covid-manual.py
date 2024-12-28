052d1361637d758bbb209dcb05b5dc0e2b494cf1
JangWoo <tjwkddn970305@gmail.com>
2024-12-28 14:58:05 +0900

    Title: 코로나 메뉴얼, Time: 76ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241228/코로나 메뉴얼/covid-manual.py
+++ 241228/코로나 메뉴얼/covid-manual.py
@@ -1,20 +1,18 @@
-# 입력값 저장
-people = []  # 각 사람의 (symptom, temperature)를 저장할 리스트
+people = []
 
-# 3명 입력받기
 for _ in range(3):
     symptom, temperature = input().split()
     temperature = int(temperature)
-    people.append((symptom, temperature))  # 튜플 형태로 저장
+    people.append((symptom, temperature))
 
-# 위급 상황 판단
 emergency_count = 0
 
 for symptom, temperature in people:
     if symptom == "Y" and temperature >= 37:
         emergency_count += 1
 
-if emergency_count >= 2:
-    print("E")  # 위급상황
+if emergency_count => 2:
+    print("E")
 else:
-    print("N")  # 정상
+    print("N")
+
