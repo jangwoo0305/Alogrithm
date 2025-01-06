a0d4d1668069b3ea81fad7406c48320ecb904897
JangWoo <tjwkddn970305@gmail.com>
2025-01-06 23:06:45 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
@@ -1,6 +1,7 @@
-N = int(input())
+n = int(input())  # 사용자로부터 숫자 입력 받음
+star = ""  # 빈 문자열로 초기화
 
-for i in range(N): # 이 반복문이 한번 돌때마다 행이 바뀐다.
-    for _ in range(i+1):
-        print("*", end="")
-    print()        
+for i in range(n):
+    star += "*"  # 별 하나 추가
+    print(star)  # 현재 문자열 출력
+   
