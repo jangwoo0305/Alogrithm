ab75389a12499cf16647b80919f5ef91a2bea91b
JangWoo <tjwkddn970305@gmail.com>
2025-01-06 23:09:28 +0900

    Update 별 찍기 － 1.py

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
@@ -4,4 +4,11 @@ star = ""  # 빈 문자열로 초기화
 for i in range(n):
     star += "*"  # 별 하나 추가
     print(star)  # 현재 문자열 출력
-   
+
+
+# N = int(input())
+
+# for i in range(N): # 이 반복문이 한번 돌때마다 행이 바뀐다.
+#     for _ in range(i+1):
+#         print("*", end="")
+#     print()      
