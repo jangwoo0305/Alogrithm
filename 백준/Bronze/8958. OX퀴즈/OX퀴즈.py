b6f3a0b5fc3ef47396793e9ed3b4e1d5aa133871
JangWoo <tjwkddn970305@gmail.com>
2025-01-09 17:08:05 +0900

    [Bronze II] Title: OX퀴즈, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+for i in range(N):
+    M = input()
+    point = 0
+    sum_point = 0  
+    for j in M:
+        if j == "O":
+            point += 1
+        else:
+            point = 0
+        sum_point += point
+    print(sum_point)
+    
+
