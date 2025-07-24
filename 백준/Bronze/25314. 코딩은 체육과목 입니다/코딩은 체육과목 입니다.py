0e5b0df50aff07790611054aad7c230af91648e4
WooJang <tjwkddn970305@gmail.com>
2025-07-24 22:11:25 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+# 4 = long int
+# 8 = long long int 
+# 12 = long long long int
+
+# N / 4 = ?
+# ? = x
+# long x
+x = int(N / 4)
+for i in range(x):
+    print("long", end=' ')
+print("int")
