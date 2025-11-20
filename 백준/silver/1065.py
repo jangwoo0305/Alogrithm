7e6c19a40e2a1711e4f34d8605f5b42e52581bcf
jangwoo0305 <tjwkddn970305@gmail.com>
2025-11-20 22:20:52 +0900

    ?

--- 백준/silver/1065.py
+++ 백준/silver/1065.py
@@ -5,5 +5,10 @@ count = 0
 for i in range(1,N+1):
     if i < 100:
         count += 1
-
+    else:
+        a = i // 100
+        b = (i // 10) % 10
+        c = i % 10
+        if (b - a) == (c - b):
+            count += 1
 print(count)
