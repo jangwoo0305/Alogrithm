a156dd36cacc6e6f50c79720c9a253d4db6da013
WooJang <tjwkddn970305@gmail.com>
2026-02-09 18:08:26 +0900

    [Bronze II] Title: 문자열 반복, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,9 +1,14 @@
-S = int(input())
-
-for _ in range(S):
-    R, S = input().split()
-    R = int(R)
-    result = ""
-    for char in S:
-        result += char * R
-    print(result)
+def alpha():
+    T = int(input())
+    
+    for _ in range(T):
+        R,S = input().split()
+        R = int(R)
+        result = ""
+        
+        for i in S:
+            result += i * R
+        print(result)
+        
+if __name__ == '__main__':
+    alpha()
