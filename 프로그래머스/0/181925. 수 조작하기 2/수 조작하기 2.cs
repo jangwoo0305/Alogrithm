ae8927286054961a032815f8e4812ca9998f1deb
WooJang <tjwkddn970305@gmail.com>
2026-08-08 10:16:20 +0900

    [level 0] Title: 수 조작하기 2, Time: 1083.93 ms, Memory: 208 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Solution 
+{
+    public string solution(int[] numLog)
+    {
+        string answer = "";
+        
+        for(int i = 0; i < numLog.Length - 1; i++)
+        {
+            int diff = numLog[i+1] - numLog[i];
+            
+            if (diff == 1)
+                answer += "w";
+            else if(diff == -1)
+                answer += "s";
+            else if(diff == 10)
+                answer += "d";
+            else
+                answer += "a";
+        }
+        
+        return answer;
+    }
+}
