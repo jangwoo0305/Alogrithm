cd7d1143365668478b1c3cc916911b7cd6176d33
WooJang <tjwkddn970305@gmail.com>
2026-09-14 21:31:46 +0900

    [Passed] 세 정수형 변수 선언 (56ms, 5MB)

---
+++ trail1/세 정수형 변수 선언/declaration-of-three-natural-numbers.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = 7, b = 23, c = 30;
+        Console.WriteLine("{0} + {1} = {2}",a,b,c);
+    }
+}
