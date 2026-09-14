8f0714ae23610a7545991564e65c6d76e89f25d4
WooJang <tjwkddn970305@gmail.com>
2026-09-14 22:15:37 +0900

    [Passed] 정수 복사 (59ms, 5MB)

---
+++ CodeTree/trail1/정수 복사/copy-integer.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = 3;
+        int b = 4;
+
+        b = a;
+        Console.WriteLine($"{a} {b}");
+        Console.WriteLine(a*b);
+
+    }
+}
