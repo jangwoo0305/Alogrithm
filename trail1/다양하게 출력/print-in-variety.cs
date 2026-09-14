874ed47e99a77546aaffafae86deea3af3565e89
WooJang <tjwkddn970305@gmail.com>
2026-09-14 21:20:48 +0900

    [Passed] 다양하게 출력 (54ms, 4MB)

---
+++ trail1/다양하게 출력/print-in-variety.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        Console.WriteLine("Total days in Year");
+        Console.WriteLine("365");
+        Console.WriteLine("Circumference rate");
+        Console.WriteLine("3.1415926535");
+    }
+}
