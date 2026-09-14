412c337b75ceeabdd0788dc008f941289c120fdb
WooJang <tjwkddn970305@gmail.com>
2026-09-14 21:50:55 +0900

    [Passed] 소수점 반올림하기 (75ms, 5MB)

---
+++ CodeTree/trail1/소수점 반올림하기/rounding-decimal-points.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        float a = 25.352f;
+
+        Console.Write($"{a:F1}");
+    }
+}
