902072f925b03f63f6fe1abe7b7f838974a70147
WooJang <tjwkddn970305@gmail.com>
2026-09-14 21:27:40 +0900

    [Passed] 변수 선언하기 (55ms, 5MB)

---
+++ trail1/변수 선언하기/declaring-variables.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = 3;
+        char letter = 'C';
+
+        Console.WriteLine(a);
+        Console.WriteLine(letter);
+    }
+}
