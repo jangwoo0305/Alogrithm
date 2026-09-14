104dcd99c8d8ac1add86aed5984ea243080938e7
WooJang <tjwkddn970305@gmail.com>
2026-09-14 22:01:12 +0900

    [Passed] 변수 값 교체하기 (58ms, 5MB)

---
+++ CodeTree/trail1/변수 값 교체하기/replacing-variable-values.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = 3;
+        a = 6;
+
+        Console.Write(a);
+    }
+}
