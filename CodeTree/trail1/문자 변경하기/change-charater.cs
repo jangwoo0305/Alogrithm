7ed2c05bebcc27a4c41509bcad8910cea1bec023
WooJang <tjwkddn970305@gmail.com>
2026-09-14 22:06:28 +0900

    [Passed] 문자 변경하기 (53ms, 4MB)

---
+++ CodeTree/trail1/문자 변경하기/change-charater.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        char a = 'C';
+        a = 'T';
+
+        Console.Write(a);
+    }
+}
