d258d010d3eb089c76539f29e052292c7f7ffc64
WooJang <tjwkddn970305@gmail.com>
2026-09-14 21:23:48 +0900

    [Passed] 정수 선언하고 뺄셈 (58ms, 4MB)

---
+++ trail1/정수 선언하고 뺄셈/define-numbers-and-substract.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = 97;
+        int b = 13;
+
+        Console.WriteLine($"{a} - {b} = {a-b}");
+    }
+}
