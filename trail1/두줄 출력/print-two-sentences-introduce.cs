11eac917873ce1484ccf371cad8f93ba5f701690
WooJang <tjwkddn970305@gmail.com>
2026-09-14 21:19:49 +0900

    [Passed] 두줄 출력 (53ms, 4MB)

---
+++ trail1/두줄 출력/print-two-sentences-introduce.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        Console.WriteLine("Hello students!");
+        Console.WriteLine("Welcome to LeebrosCode!");
+    }
+}
