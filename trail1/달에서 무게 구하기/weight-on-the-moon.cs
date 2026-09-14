caf23fcc1a67f9a56b00d2373bae7778388400d4
WooJang <tjwkddn970305@gmail.com>
2026-09-14 21:46:27 +0900

    [Passed] 달에서 무게 구하기 (76ms, 5MB)

---
+++ trail1/달에서 무게 구하기/weight-on-the-moon.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = 13;
+        float moonGravityratio = 0.165f;
+        float result = 13 * 0.165f;
+
+        Console.WriteLine($"{a} * {moonGravityratio:F6} = {result:F6}");
+    }
+}
