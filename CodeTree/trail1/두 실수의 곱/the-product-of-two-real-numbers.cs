bc2bb7867de019f033aa6e64458180e3f2523eaf
WooJang <tjwkddn970305@gmail.com>
2026-09-14 21:59:18 +0900

    [Passed] 두 실수의 곱 (76ms, 5MB)

---
+++ CodeTree/trail1/두 실수의 곱/the-product-of-two-real-numbers.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        float a = 5.26f;
+        float b = 8.27f;
+
+        Console.WriteLine($"{a*b:F3}");
+    }
+}
