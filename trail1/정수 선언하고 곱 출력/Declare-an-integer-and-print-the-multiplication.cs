ec71e4d387bfc02700b09532cb2e7225a7cb52fa
WooJang <tjwkddn970305@gmail.com>
2026-09-14 21:29:01 +0900

    [Passed] 정수 선언하고 곱 출력 (58ms, 5MB)

---
+++ trail1/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = 26 , b = 5;
+        Console.WriteLine($"{a} * {b} = {a*b}");
+    }
+}
