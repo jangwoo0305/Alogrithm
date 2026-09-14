d2e082d6ce5e9cb9b8b03a7df151dabb86736c3c
WooJang <tjwkddn970305@gmail.com>
2026-09-14 21:00:13 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 33.54 ms, Memory: 29.5 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.cs
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.cs
@@ -5,7 +5,6 @@ public class Example
     public static void Main()
     {
         String str = Console.ReadLine();
-        String Resultstr = string.Empty;
         
         for (int i = 0; i < str.Length; i++)
         {
