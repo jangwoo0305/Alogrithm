643d960e7431ff42ad54d48d14a0bde93e4b739b
WooJang <tjwkddn970305@gmail.com>
2026-09-14 21:36:08 +0900

    [Passed] 변수 출력하기 2 (57ms, 4MB)

---
+++ trail1/변수 출력하기 2/outputing-variables-2.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = 3;
+        char letter = 'C';
+
+        Console.WriteLine($"{letter}!.....!{a}");
+    }
+}
